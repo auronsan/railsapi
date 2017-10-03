907b4f05e026b3b092891b4864265332b45850ae
Ahsanul <Developer@Developers-Mac-mini.local>
2017-10-03 16:50:47 +0700

    rails api

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,9 @@
 
 require 'rails_helper'
 
-# Test suite for the Todo model
 RSpec.describe User, type: :model do
  
   # Validation tests
-  # ensure columns title and created_by are present before saving
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:created_by) }
